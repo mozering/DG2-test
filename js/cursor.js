6e039fe5fa8c578a557a44c189e9047f1cffc961
kimhyesoo <lazypumpkin2@gmail.com>
2021-01-28 03:04:16 +0900

    https://github.com/twbs/bootstrap.git

---
+++ js/cursor.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	$(document).mousemove(function(e){
+		var mouseX = e.pageX;
+		var mouseY = e.pageY;
+		$(".cursor").css({
+			left:mouseX+"px",
+			top:mouseY+"px"
+		});
+
+	});
+
+});
